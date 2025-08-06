ac05e5b6de1e24e1013d1d81baa6cf704a03282a
Mateus Marques <203121612+gn-mm@users.noreply.github.com>
2025-08-06 11:51:24 -0300

    Problem 2120C: My solution was correct, but I/O was horrible. DeepSeek optimize it for Fast I/O

--- matmarqs/codeforces/2120C-divine_tree.c
+++ matmarqs/codeforces/2120C-divine_tree.c
@@ -1,5 +1,51 @@
 #include <stdio.h>
 
+/* fast I/O, because I got Time Limit Exceeded */
+static char outbuf[8<<20];
+static int outidx = 0;
+
+void flush_output() {
+    if (outidx) {
+        fwrite(outbuf, 1, outidx, stdout);
+        outidx = 0;
+    }
+}
+
+void fast_write_int(int x) {
+    if (x == 0) {
+        if (outidx == sizeof(outbuf)) flush_output();
+        outbuf[outidx++] = '0';
+        return;
+    }
+    char temp[12];
+    int len = 0;
+    int is_negative = 0;
+    if (x < 0) {
+        is_negative = 1;
+        x = -x;
+    }
+    while (x) {
+        temp[len++] = x % 10 + '0';
+        x /= 10;
+    }
+    if (outidx + len + is_negative > sizeof(outbuf)) {
+        flush_output();
+    }
+    if (is_negative) {
+        outbuf[outidx++] = '-';
+    }
+    for (int i = len-1; i >= 0; i--) {
+        outbuf[outidx++] = temp[i];
+    }
+}
+
+void fast_write_char(char c) {
+    if (outidx == sizeof(outbuf)) {
+        flush_output();
+    }
+    outbuf[outidx++] = c;
+}
+
 /* this is the maximum total divineness of the tree, if the root label is k */
 /* f(n, k) is monotonic increasing in 1 <= k <= n */
 /* f(n, 1) = n */
@@ -10,32 +56,36 @@ long long f(long long n, long long k) {
 
 /* optimal_k = min { k \in {1, 2, ..., n} | m <= f(n, k) } */
 /* we assume that f(n,1) <= m <= f(n,n) */
-int find_optimal_k(int n, long long m) {    /* binary search method */
+int find_optimal_k(int n, long long m) {
     int left = 1;
     int right = n;
     int k;
     while (1) {
         k = (left + right) / 2;
-        if (m <= f(n, k)) {
-            if (m > f(n, k-1)) {
-                break;
+        long long fk = f(n, k);
+        if (m <= fk) {
+            long long fk_prev = 0;
+            if (k > 1) {
+                fk_prev = f(n, k-1);
             }
-            else {
+            if (m > fk_prev) {
+                break;
+            } else {
                 right = k-1;
             }
-        }
-        else {
+        } else {
             left = k+1;
         }
     }
     return k;
 }
 
-void solve(int n, long long m, int *parent) {
+void solve(int n, long long m) {
     /* minimum of m = n */
     /* maximum of m = n*(n+1)/2 */
     if (m < f(n, 1) || m > f(n, n)) {
-        printf("%d\n", -1);
+        fast_write_int(-1);
+        fast_write_char('\n');
         return;
     }
 
@@ -54,45 +104,45 @@ void solve(int n, long long m, int *parent) {
     /* and their divineness is the parent to which they were attached */
     /* therefore, we need to find any partition */
     /* q_{k+1} + q_{k+2} + ... + q_{n} = q = m - k(k+1)/2; */
-    long long q = m - (k*(k+1))/2;
 
-    int num_values_to_find = n - k;
-    /* O(n) */
-    for (int i = 0; i < num_values_to_find; i++) {
-        int rem = num_values_to_find - i - 1;   /* how many remaining values we still have to choose after this one */
-        long long min_val = q - rem * k;    /* minimum value we can assign to this position */
-        /* Each of those can be at most k, so rem * k is the maximum the rest can sum to */
-        if (min_val < 1) min_val = 1;
-        if (min_val > k) min_val = k;
-        /* Clamp min_val to the range [1, k], since each value must stay in that range. */
-        parent[i] = min_val;
-        q -= min_val;
+    /* output the root of the tree */
+    fast_write_int(k);
+    fast_write_char('\n');
+
+    /* output vertices 1, 2, ..., k-1 */
+    /* they are direct children of the root */
+    for (int i = 1; i < k; i++) {
+        fast_write_int(k);
+        fast_write_char(' ');
+        fast_write_int(i);
+        fast_write_char('\n');
     }
 
-    // Output the result
-    printf("%d\n", k);  // root of the tree
-    for (int i = 1; i <= n; i++) {
-        if (i == k) {   /* the root */
-            continue;
-        }
-        if (i < k) {    /* vertices 1, 2, ..., k-1 */
-            printf("%d %d\n", k, i);    /* they are direct children of the root */
-        }
-        else {  /* vertices k+1, k+2, ..., n */
-            printf("%d %d\n", parent[i-(k+1)], i);    /* they direct children of the assigned partition */
-        }
+    long long q_val = m - (long long)k * (k+1) / 2;
+    for (int j = k+1; j <= n; j++) {
+        int rem = n - j;    /* how many remaining values we still have to choose after this one */
+        long long min_val = q_val - (long long)rem * k; /* minimum value we can assign to this position */
+        /* Each of those can be at most k, so rem * k is the maximum the rest can sum to */
+        /* Clamp min_val to the range [1, k], since each value must stay in that range. */
+        if (min_val < 1) min_val = 1;
+        if (min_val > k) min_val = k;
+        q_val -= min_val;
+
+        fast_write_int((int)min_val);
+        fast_write_char(' ');
+        fast_write_int(j);
+        fast_write_char('\n');
     }
 }
 
 int main() {
     int t, n;
     long long m;
-    int parent[(int) 1e6];
     scanf("%d", &t);
-    for (; t > 0; t--) {
+    while (t--) {
         scanf("%d %lld", &n, &m);
-        solve(n, m, parent);
+        solve(n, m);
     }
+    flush_output(); /* fast I/O, because I got Time Limit Exceeded */
     return 0;
 }
-
